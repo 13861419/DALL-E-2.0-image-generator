9bcb4440355c96f3c869dab3144c156da8a66da1
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-25 01:52:07 +0530

    (config): added openai config file

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,16 +1,27 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 function PromptInput() {
+  const [input, setInput] = useState("");
   return (
     <div className="m-10">
       <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
         <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           placeholder="Enter a prompt..."
           className="flex-1 p-4 outline-none rounded-md"
         />
-        <button type="submit" className="">
-          Genrerate
+        <button
+          type="submit"
+          disabled={!input}
+          className={`p-4 font-bold ${
+            input
+              ? "bg-violet-500 text-white transition-colors duration-200"
+              : "text-gray-300 cursor-not-allowed"
+          }`}
+        >
+          Generate
         </button>
         <button
           type="button"
