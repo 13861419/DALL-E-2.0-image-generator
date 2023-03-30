400b4e2e73c7ba7245aafaec243493c1f3f667c7
jack <13564034268@139.com>
2023-03-30 13:38:56 +0800

    代码提交

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -97,14 +97,14 @@ function PromptInput() {
         >
           生成
         </button>
-        <button
+        {/* <button
           className={`p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400`}
           onClick={() => submitPrompt(true)}
           disabled={isLoading || isValidating}
           type="button"
         >
           使用建议
-        </button>
+        </button> */}
         {/* <button
           className={`p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold`}
           onClick={mutate}
